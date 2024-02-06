8cd919988092ff8b41aa6e4a76b14050797c4c4d
Riley <145041122+Riley-3@users.noreply.github.com>
2024-02-06 13:55:53 -0600

    Initial unit test project

---
+++ Riley033.TestDeploymentRepo/Riley033.TestDeploymentRepo.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Riley033.TestDeploymentRepo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
