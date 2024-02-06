ba830974b5714635a6ff8c58596dad98265033de
Riley <145041122+Riley-3@users.noreply.github.com>
2024-02-06 13:58:56 -0600

    Add basic test for adding 2 numbers

--- Riley033.TestDeploymentRepo/Riley033.TestDeploymentRepo.Tests/UnitTest1.cs
+++ Riley033.TestDeploymentRepo/Riley033.TestDeploymentRepo.Tests/UnitTest1.cs
@@ -3,9 +3,14 @@ namespace Riley033.TestDeploymentRepo.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 2, 4)]
+        [DataRow(1, 3, 4)]
+        [DataRow(-1, 1, 0)]
+        public void TestMethod1(int x, int y, int expected)
         {
+            Assert.AreEqual(expected, Class1.Add(x, y));
         }
     }
 }
